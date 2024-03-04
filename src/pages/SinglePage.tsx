f0003bf0b789323a5841965c7941fddf8f00b83c
Success Enehikhuere <enehikhueresuccess@gmail.com>
2024-03-04 20:47:24 +0100

    "update codes in SinglePage"

--- src/pages/SinglePage.tsx
+++ src/pages/SinglePage.tsx
@@ -2,44 +2,43 @@ import React from 'react'
 import { Link, To } from 'react-router-dom'
 
 interface newsArticles {
-  url: To
+  url: any
   author: any
   urlToImage: any
   title: any
+  description: any
   article: {
     author: any
     title: string
-    description: string
-    url: string
     publishedAt: string
     urlToImage: string
   }[]
 }
 
 
+
 const SinglePage = (article: newsArticles) => {
   return (
-    <div>
-      <Link
-        to={article.url}
-        className="w-full shadow-xl hover:shadow-2xl transition duration-300"
-      >
-        <div>
-          <figure>
-            <img
-              src={article.urlToImage}
-              alt={article.title}
-              className="rounded-xl h-64 md:h-48  object-cover mx-auto"
-            />
-          </figure>
-          <div className="card-body items-center text-center">
-            <h2 className="card-title capitalize tracking-wider">
-              {article.title}
-            </h2>
-            <span className="text-secondary">{article.author}</span>
-          </div>
-        </div>
-      </Link>
+    <div className="w-full shadow-md hover:shadow-lg transition duration-300">
+      <figure>
+        <img
+          src={article.urlToImage}
+          alt={article.title}
+          className="rounded-xl h-64 md:h-48  object-cover mx-auto"
+        />
+      </figure>
+      <div className="items-center text-center">
+        <h2 className="capitalize tracking-wider">{article.title}</h2>
+        <p>
+          <span className="text-secondary">{article.author}</span>
+        </p>
+        <p>
+          <span>{article.description}</span>
+          <a href={article.url} target="_blank">
+            <button>read more</button>
+          </a>
+        </p>
+      </div>
     </div>
   )
 }
