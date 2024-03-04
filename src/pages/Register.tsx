bdc1d28f3e00780acbe71aebdb5cc60bddbf20ec
Success Enehikhuere <enehikhueresuccess@gmail.com>
2024-03-04 20:48:07 +0100

    " set up login page, and register page"

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,28 +1,30 @@
 import React from 'react'
-import { Form } from 'react-router-dom'
-import { Link } from 'react-router-dom'
 import { FormInput, SubmitBtn } from '../components'
+import { Form, Link} from 'react-router-dom'
 
-const Login = () => {
-  const userLogin = () => {}
+
+const Register = () => {
   return (
     <section className="h-screen grid place-items-center">
       <Form
         method="POST"
         className="card w-96 p-8 bg-base-100 shadow-lg flex flex-col gap-y-4 capitalize"
       >
-        <h4 className="text-center text-3xl font-bold">Login</h4>
+        <h4 className="text-center text-3xl font-bold">register</h4>
+        <FormInput type="text" name="username" label="name" />
         <FormInput type="email" name="email" label="email" />
         <FormInput type="password" name="password" label="password" />
         <div className="mt-4">
-          <Link to='/'>
-            <SubmitBtn text="login" />
-          </Link>
+          <SubmitBtn text="submit" />
         </div>
+
         <p className="text-center">
-          not a member yet?
-          <Link to="/register" className="ml-2 link link-hover link-primary font-bold">
-            register
+          already a member?
+          <Link
+            to="/login"
+            className="ml-2 link link-hover link-primary font-bold"
+          >
+            login
           </Link>
         </p>
       </Form>
@@ -30,4 +32,4 @@ const Login = () => {
   )
 }
 
-export default Login
+export default Register
