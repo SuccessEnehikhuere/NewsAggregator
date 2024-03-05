8734f17d7550951174be14de70cf60d8bec5d0eb
Success Enehikhuere <enehikhueresuccess@gmail.com>
2024-03-05 22:45:46 +0100

    "add more functionalities to the submitBtn component"

--- src/components/SubmitBtn.tsx
+++ src/components/SubmitBtn.tsx
@@ -1,31 +1,33 @@
-import React from 'react'
+import React, { FC, MouseEvent } from 'react'
 import { useNavigation } from 'react-router-dom'
 
 interface submitBtn {
-  size ?: string
+  size?: string
   text: string
+  onClick: (e: MouseEvent<HTMLButtonElement>) => void
 }
 
-const SubmitBtn = ({size, text}:submitBtn) => {
-    const navigation = useNavigation()
-    const isSubmitting = navigation.state === 'submitting'
+const SubmitBtn = ({ size, text, onClick }: submitBtn) => {
+  const navigation = useNavigation()
+  const isSubmitting = navigation.state === 'submitting'
 
-    return (
-      <button
-        type="submit"
-        className={`btn bg-[#4f46e5] btn-block capitalize text-white hover:bg-[#4338ca] 
+  return (
+    <button
+      type="submit"
+      className={`btn bg-[#4f46e5] btn-block capitalize text-white hover:bg-[#4338ca] 
       ${size}`}
-        disabled={isSubmitting}
-      >
-        {isSubmitting ? (
-          <>
-            <span className="loading loading-spinner"></span>loading...
-          </>
-        ) : (
-          text || 'submit'
-        )}
-      </button>
-    )
+      disabled={isSubmitting}
+      onClick={onClick}
+    >
+      {isSubmitting ? (
+        <>
+          <span className="loading loading-spinner"></span>loading...
+        </>
+      ) : (
+        text || 'submit'
+      )}
+    </button>
+  )
 }
 
 export default SubmitBtn
