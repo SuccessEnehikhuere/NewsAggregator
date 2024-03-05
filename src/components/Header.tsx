076785d54a106d945cb38ee2ba5b6ee111202c2d
Success Enehikhuere <enehikhueresuccess@gmail.com>
2024-03-05 22:49:28 +0100

    "set up themes functionality to the header component"

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,37 +1,52 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import { BsMoonFill, BsSunFill } from 'react-icons/bs'
+
+const themes = {
+  winter: 'winter',
+  dracula: 'dracula',
+}
+
+const getThemeFromLocalStorage = () => {
+  return localStorage.getItem('theme') || themes.winter
+}
 
 const Header = () => {
+  //setting and saving theme based on user's preference
+  const [theme, setTheme] = useState(getThemeFromLocalStorage())
+  const handleTheme = () => {
+    const { winter, dracula } = themes
+    const newTheme = theme === winter ? dracula : winter
+    setTheme(newTheme)
+  }
+  
+   useEffect(() => {
+     document.documentElement.setAttribute('data-theme', theme)
+     localStorage.setItem('theme', theme)
+   }, [theme]) // this runs whenever theres a change in the theme
+
+
   return (
-    <header className="bg-neutral-content text-black py-2 z-[1] ">
-      <div className="align-item flex justify-center sm:justify-end ">
-        {/* USER
-        {user ? (
-          <div className="flex gap-x-2 sm:gap-x-8 items-center">
-            <p className="text-xs sm:text-sm">Hello, {user.username}</p>
-            <button
-              className="btn btn-xs  bg-[#4f46e5] hover:bg-[#a5b4fc] text-white border-none"
-              onClick={handleUserLogout}
-            >
-              logout
-            </button>
-          </div>
-        ) : ( */}
-          <div className="flex gap-x-6 justify-center items-center ">
-            <Link
-              to="/login"
-              className="link link-hover text-xs sm:text-sm capitalize"
-            >
-              sign in/ guest
-            </Link>
-            <Link
-              to="/register"
-              className="link link-hover text-xs sm:text-sm capitalize"
-            >
-              create account
-            </Link>
-          </div>
-        {/* )} */}
+    <header className="bg-neutral-content text-black py-2 z-[1] pr-6 ">
+      <div className="align-item flex justify-end ">
+        <div className="flex gap-x-6 justify-center items-center ">
+          <Link
+            to="/"
+            className="link link-hover text-xs sm:text-sm capitalize"
+          >
+            sign out
+          </Link>
+          <label className="swap swap-rotate">
+          <input type="checkbox" onChange={handleTheme} />
+          <BsSunFill className="swap-on h-4 w-4" />
+          <BsMoonFill className="swap-off h-4 w-4" />   
+        </label>
+
+        </div>
+        <div>
+        </div>
+        
       </div>
     </header>
   )
