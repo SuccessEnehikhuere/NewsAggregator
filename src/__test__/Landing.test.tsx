1acb2e3b9dcf418f3e4a24e45f92c77fe994d23a
Success Enehikhuere <enehikhueresuccess@gmail.com>
2024-03-06 18:57:41 +0100

    "removed unused file"

--- src/__test__/Landing.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render, fireEvent, waitFor } from '@testing-library/react'
-import axios from 'axios'
-import { Landing } from '../pages'
-import jest from 'jest'
-
-
-
-
